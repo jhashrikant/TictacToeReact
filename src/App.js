2435796d0c6c19c8e1a453fb957847ca8d996d43
Shrikant Jha <87967393+jhashrikant@users.noreply.github.com>
2023-12-15 15:40:09 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -8,17 +8,18 @@ function App() {
 	const [state, setState] = useState(Array(9).fill(null))
 	const [currentPlayer, setcurrentplayer] = useState('X')
 
+	const Winningprobablity = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	]
+
 	function checkWinner() {
-		const Winningprobablity = [
-			[0, 1, 2],
-			[3, 4, 5],
-			[6, 7, 8],
-			[0, 3, 6],
-			[1, 4, 7],
-			[2, 5, 8],
-			[0, 4, 8],
-			[2, 4, 6],
-		]
 		for (let i = 0; i < Winningprobablity.length; i++) {
 			const [a, b, c] = Winningprobablity[i]
 			console.log(a, b, c);
